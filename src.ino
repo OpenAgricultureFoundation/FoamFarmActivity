9a20d50a93724d1160d612df9a720b5fe5d9b88b
Leon Chambers <leon8556@gmail.com>
2016-07-19 13:32:27 -0400

    Initial commit

---
+++ src.ino
@@ -0,0 +1,81 @@
+#define LIGHT 3
+#define FAN 4
+#define HUMIDIFIER 5
+#define PUMP 6
+
+#include <Wire.h>
+#include "Adafruit_AM2315.h"
+
+const int lightOnPeriod = 10000;
+const int lightOffPeriod = 5000;
+
+bool isLightOn = false;
+
+int lightTurnedOn = 0;
+int lightTurnedOff = 0;
+
+const int desiredTemperature = 22;
+const int desiredHumidity = 47;
+
+Adafruit_AM2315 am2315;
+
+float currentTemperature;
+float currentHumidity;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(FAN, OUTPUT);
+  pinMode(HUMIDIFIER, OUTPUT);
+  pinMode(LIGHT, OUTPUT);
+  pinMode(PUMP, OUTPUT);
+
+  if (!am2315.begin()) {
+    Serial.println("Failed to initialize am2315");
+    while (true);
+  }
+}
+
+void loop() {
+  int ms = millis();
+
+  // If the light has been on for too long, turn it off
+  if (isLightOn) {
+    if (ms - lightTurnedOn > lightOnPeriod) {
+      lightTurnedOff = lightTurnedOn + lightOnPeriod;
+      isLightOn = false;
+      digitalWrite(LIGHT, LOW);
+    }
+  }
+  // If the light has been off for too long, turn it on
+  else {
+    if (ms - lightTurnedOff > lightOffPeriod) {
+      lightTurnedOn = lightTurnedOff + lightOffPeriod;
+      isLightOn = true;
+      digitalWrite(LIGHT, HIGH);
+    }
+  }
+
+  if (!am2315.readTemperatureAndHumidity(currentTemperature, currentHumidity)) {
+    Serial.println("Failed to read from am2315");
+  }
+  else {
+    Serial.print("Air Temperature: ");
+    Serial.println(currentTemperature);
+    Serial.print("Air Humidity: ");
+    Serial.println(currentHumidity);
+    if (desiredHumidity > currentHumidity) {
+      digitalWrite(HUMIDIFIER, HIGH);
+    }
+    else {
+      digitalWrite(HUMIDIFIER, LOW);
+    }
+    if (desiredTemperature < currentTemperature) {
+      digitalWrite(FAN, HIGH);
+    }
+    else {
+      digitalWrite(FAN, LOW);
+    }
+  }
+  delay(500);
+}
